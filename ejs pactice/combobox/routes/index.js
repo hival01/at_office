3af7340dc2453e90bbb37ee533e01eb25ab0ce50
hival01 <hivalpatel9219@gmail.com>
2026-03-25 13:24:45 +0530

    combobox final

--- ejs pactice/combobox/routes/index.js
+++ ejs pactice/combobox/routes/index.js
@@ -1,15 +1,49 @@
 const express = require("express");
 const db = require("../config/database");
-
 const router = express.Router();
-const country = ["Kano"];
 
 router.get("/", async (req, res) => {
-  for (let i = 0; i < country.length; i++) {
-    let dbquery = `insert into city (state_id ,city_name) values ((select state_id from state where state_name="Kano") ,"${country[i]}")`;
-    const [data] = await db.execute(dbquery);
+  res.render("form");
+});
+
+router.get("/api/country", async (req, res) => {
+  try {
+    const data = await db.execute("select * from country order by country_id");
+    res.json(data[0]);
+  } catch (err) {
+    console.log(err);
+    throw err;
   }
-  res.send("no");
+});
+
+router.get("/api/state/:country_id", async (req, res) => {
+  const country_id = req.params.country_id;
+  try {
+    const data = await db.execute(
+      `select * from state where country_id=${country_id} order by state_id`,
+    );
+    res.json(data[0]);
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+});
+
+router.get("/api/city/:state_id", async (req, res) => {
+  const state_id = req.params.state_id;
+  try {
+    if (!state_id) {
+      console.log("select the state first");
+    } else {
+      const data = await db.execute(
+        `select * from city where state_id=${state_id} order by city_id`,
+      );
+      res.json(data[0]);
+    }
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
 });
 
 module.exports = router;
--- ejs pactice/combobox/server.js
+++ ejs pactice/combobox/server.js
@@ -2,13 +2,16 @@ const express = require("express");
 require('dotenv').config();
 
 const app= express();
-
 app.set("view engine" , "ejs");
+
+// app.use(express.json);
+// app.use(express.urlencoded({ extended: true })); 
+
 
 app.use(express.static("public"))
 
-const itemInsert = require("./routes/index")
-app.use("/insert" , itemInsert);
+const itemInsert = require("./routes")
+app.use("/" , itemInsert);
 
 app.listen(process.env.port || 3000 , ()=>{
     console.log(`server is runnig on http://localhost:${process.env.port ||3000}`);   
